2f47cd321adcf422d4ca7f31181d5007a2ef4ded
Sabyasachee <61593726+Sabya-sudo@users.noreply.github.com>
2024-02-29 00:27:44 +0530

    Array question
    Adding 2 arrays and storing it into 3rd array

---
+++ arraylearn.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n;
+    cin>>m;
+    int mer=n+m;
+    int arr1[n],arr2[m],arr3[mer];
+    //input of 1st array
+    for(int i=0;i<n;i++){
+        cin>>arr1[i];
+    }
+    //input for 2nd array
+    for(int i=0;i<n;i++){
+        cin>>arr2[i];
+    }
+    //adding these two in 3rd array
+    for(int i=0;i<n;i++){
+        arr3[i]=arr1[i];
+    }
+
+     for(int i=0;i<n;i++){
+        arr3[n+i]=arr2[i];
+    }
+    for (int i = 0; i < mer; ++i) {
+        cout << arr3[i] << " ";
+    }
+    return 0;
+}

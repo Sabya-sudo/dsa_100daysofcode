04cdc9a3287fc205d40166ed7b78ea1206c44097
Sabyasachee <61593726+Sabya-sudo@users.noreply.github.com>
2024-04-01 23:34:43 +0530

    Update day2.cpp
    Added stack question without implementing array

--- day2.cpp
+++ day2.cpp
@@ -20,3 +20,58 @@ for(int i=0;i<n;i++){
 cout<<sum;
     return 0;
 }
+
+//Stack question without implementing array will edit it later
+#include<iostream>
+using namespace std;
+int main(){
+    
+int arr[10]={0};
+    
+int n;
+
+int count =9 ;
+while(true){
+   cin>>n; 
+int a;
+
+if(n==1){
+    cin>>a;
+    arr[count]=a;
+    count--;
+    cout<<"Element "<<a<<" pushed onto the stack."<<endl;
+    
+}
+    else if(n==2){
+        if(count<=9 || arr[count]!=0){
+        cout<<"Element "<<arr[count + 1]<<" Popped from the stack."<<endl;
+        arr[count + 1]=0;
+        count++;}
+        else if(count>9){
+            cout<<"Stack Underflow. Cannot perform pop operation.";
+        }
+    }
+    else if(n==3){
+        if(count == 9){
+            cout<<"Stack is empty."<<endl;
+        }
+        else {
+        cout<<"Elements in the stack: ";
+        for(int i=0;i<10;i++){
+            if(arr[i] != 0){
+            cout<<arr[i]<<" ";}
+        }
+        cout<<endl;}
+    }
+    else if(n==4){
+        cout<<"Exiting the program."<<endl;
+        break;
+    }
+    else{
+        cout<<"Invalid choice."<<endl;
+    }
+     
+    
+}
+    return 0;
+}

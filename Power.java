2ee26f24630ed27da7cac49943c8bd987ae32a61
Sabyasachee <61593726+Sabya-sudo@users.noreply.github.com>
2024-04-26 12:38:05 +0530

    Program to find out how many times a number can be divided by another number.

---
+++ Power.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Power {
+    public static void main(String[] args){
+        int count=0;
+        int sum=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int number = sc.nextInt();
+        System.out.println("Enter N");
+        int n = sc.nextInt();
+        while (number % n == 0) {
+            count++;
+            number = number/n;
+        }
+        System.out.println(count);
+        
+    }
+    
+}

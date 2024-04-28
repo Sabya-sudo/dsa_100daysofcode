4cae8c9a0783957489709d9bd9d935fe885069f8
Sabyasachee <61593726+Sabya-sudo@users.noreply.github.com>
2024-04-28 22:31:45 +0530

    accumlate.cpp

--- accumlate.cpp
+++ accumlate.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main (){
-   int arr[] = { 2, 5, 7, 8, 2,9,76,75,79,87,89,90,97};
+   int arr[] = { 2, 5, 7, 8, 2,9,76,75,79,87,89,90,97,99};
    int n = 7, sum = 0;
    sum = accumulate(arr, arr+n, sum);
    cout<<"The array sum is "<<sum;
